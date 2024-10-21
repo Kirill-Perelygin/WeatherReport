ee9cc5de0c6600e477f8233c43b204b38548af89
Kirill-Perelygin <perelygin.kirill@gmail.com>
2024-10-21 21:04:51 +0300

    Sending lat&lon and getting results

--- src/main/java/WeaterReport.java
+++ src/main/java/WeaterReport.java
@@ -3,11 +3,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class WeaterReport {
 
-    static int optionChoice;A
+    static int optionChoice;
     static String lon;
     static String lat;
 
@@ -30,13 +32,12 @@ public class WeaterReport {
                 lat = scanner1.nextLine();
 
                 HttpClient client = HttpClient.newHttpClient();
-                URI uri = URI.create("https://api.weather.yandex.ru/v2/forecast?");
+                URI uri = URI.create("https://api.weather.yandex.ru/v2/forecast?lat=" + lat + "&lon=" + lon);
                 HttpRequest request = HttpRequest.newBuilder()
                         .uri(uri)
-                        .header("X-Yandex-Weather-Key", "da24c7b5-4aae-4552-860a-e3fa91f7f959")
+                        .header("X-Yandex-Weather-Key", "b1888297-4513-4390-8eb5-09286bf69278")
                         .GET()
                         .build();
-
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
                 System.out.println("The response is: " + response.body());
                 break;
