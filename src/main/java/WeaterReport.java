a83cc3061d53441191a6f957c5e8819197a7f57a
Kirill Perelygin <perelygin.kirill@gmail.com>
2024-10-27 14:58:46 +0300

    Starting to use Jackson to get factual temperature

--- src/main/java/WeaterReport.java
+++ src/main/java/WeaterReport.java
@@ -1,4 +1,8 @@
-import java.io.IOException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -26,9 +30,9 @@ public class WeaterReport {
         switch (optionChoice) {
             case 1:
                 Scanner scanner1 = new Scanner(System.in);
-                System.out.println("Введите долготу: ");
+                System.out.println("Insert longitude as hh.ss: ");
                 lon = scanner1.nextLine();
-                System.out.println("Введите широту: ");
+                System.out.println("Insert latitude as hh.ss: ");
                 lat = scanner1.nextLine();
 
                 HttpClient client = HttpClient.newHttpClient();
@@ -39,8 +43,13 @@ public class WeaterReport {
                         .GET()
                         .build();
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                System.out.println("The response is: " + response.body());
-                break;
+                String WeaterResponse = response.body();
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode jsonResponse = mapper.readTree(WeaterResponse);
+                JsonNode factTemp = jsonResponse.get("fact");
+                int temp = factTemp.get("temp").asInt();
+                System.out.println("Server response equals to: " + jsonResponse);
+                System.out.println("Factual temperature equals to: " + temp);
 
         }
 
